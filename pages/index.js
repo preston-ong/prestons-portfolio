553fc081f0a264002fbf5ea60248aac1061fb9b6
Preston Ong <prestonojb@gmail.com>
2022-07-14 17:25:43 +0800

    Initialised portfolio site with libraries and homepage

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,12 @@ import utilStyles from "../styles/utils.module.css";
 
 import { getSortedPostsData } from "../lib/posts";
 import Link from "next/link";
-import Date from "../components/date";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGithub,
+  faTwitter,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -31,7 +36,7 @@ export default function Home({ allPostsData }) {
         </p>
         <p>Drop me a message if you are keen to chat!</p>
       </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+      {/* <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
@@ -46,6 +51,48 @@ export default function Home({ allPostsData }) {
             </li>
           ))}
         </ul>
+      </section> */}
+      <section
+        className={utilStyles.textCenter}
+        style={{ marginBottom: "8px" }}
+      >
+        <Link href="/projects">
+          <a className={utilStyles.marginRight8px}>Projects</a>
+        </Link>
+        <Link href="/blog">Blog</Link>
+      </section>
+
+      <section className={utilStyles.textCenter}>
+        <a
+          style={{ color: "black", marginRight: "8px" }}
+          href="https://github.com/prestonojb"
+          target="_blank"
+        >
+          <FontAwesomeIcon
+            icon={faGithub}
+            style={{ width: "24px", height: "24px" }}
+          />
+        </a>
+        <a
+          style={{ color: "black", marginRight: "8px" }}
+          href="https://twitter.com/preston_ojb"
+          target="_blank"
+        >
+          <FontAwesomeIcon
+            icon={faTwitter}
+            style={{ width: "24px", height: "24px" }}
+          />
+        </a>
+        <a
+          style={{ color: "black" }}
+          href="https://linkedin.com/in/preston-ong-jin-bin"
+          target="_blank"
+        >
+          <FontAwesomeIcon
+            icon={faLinkedin}
+            style={{ width: "24px", height: "24px" }}
+          />
+        </a>
       </section>
     </Layout>
   );
